e8ab0944bbdf7f746e7145f7a4857694108effb7
Timóteo Stifft <timoteostifft@gmail.com>
2021-11-29 07:50:24 -0300

    feature: create dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 // import styled from 'styled-components'
 import { GlobalSyle } from './styles/global';
 import { Header } from './components/header/index'
+import { Dashboard } from './components/Dashboard';
 
 
 export function App() {
   return (
     <div className="App">
-      <>
       <Header/>
+      <Dashboard/>
       <GlobalSyle/>
-      </>
     </div>
   );
 }
